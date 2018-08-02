edd83d508d180fa363118a6d1ee4d6badd8105d2
iloktionov <iloktionov@skbkontur.ru>
2018-08-02 23:07:39 +0500

    Okay, 100k was probably too much for a queue.

--- Vostok.Logging.ConsoleLog/ConsoleLogGlobalSettings.cs
+++ Vostok.Logging.ConsoleLog/ConsoleLogGlobalSettings.cs
@@ -5,7 +5,7 @@ namespace Vostok.Logging.ConsoleLog
     [PublicAPI]
     public class ConsoleLogGlobalSettings
     {
-        public int EventsQueueCapacity { get; set; } = 100 * 1000;
+        public int EventsQueueCapacity { get; set; } = 10 * 1000;
 
         public int OutputBufferSize { get; set; } = 65536;
     }
