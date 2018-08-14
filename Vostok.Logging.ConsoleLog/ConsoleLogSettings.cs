f7653a217475a044f08daee4965f47aebcd61cc2
Krait <krait@skbkontur.ru>
2018-08-14 15:49:00 +0500

    Added xml-docs.

--- Vostok.Logging.ConsoleLog/ConsoleLogSettings.cs
+++ Vostok.Logging.ConsoleLog/ConsoleLogSettings.cs
@@ -6,13 +6,25 @@ using Vostok.Logging.Formatting;
 
 namespace Vostok.Logging.ConsoleLog
 {
+    /// <summary>
+    /// Settings for a <see cref="ConsoleLog"/> instance.
+    /// </summary>
     [PublicAPI]
     public class ConsoleLogSettings
     {
+        /// <summary>
+        /// The <see cref="OutputTemplate"/> used to render log messages.
+        /// </summary>
         public OutputTemplate OutputTemplate { get; set; } = OutputTemplate.Default;
 
+        /// <summary>
+        /// If specified, this <see cref="IFormatProvider"/> will be used when formatting log events.
+        /// </summary>
         public IFormatProvider FormatProvider { get; set; }
 
+        /// <summary>
+        /// Mapping of log levels to text color in console.
+        /// </summary>
         public Dictionary<LogLevel, ConsoleColor> ColorMapping { get; set; } = new Dictionary<LogLevel, ConsoleColor>
         {
             [LogLevel.Debug] = ConsoleColor.Gray,
@@ -22,6 +34,9 @@ namespace Vostok.Logging.ConsoleLog
             [LogLevel.Fatal] = ConsoleColor.DarkRed,
         };
 
+        /// <summary>
+        /// Specifies whether the console log must colorize text depending on the log level.
+        /// </summary>
         public bool ColorsEnabled { get; set; } = true;
     }
 }
