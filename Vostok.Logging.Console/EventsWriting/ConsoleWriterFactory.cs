4ad10d8eba65be86134e7763278167e48bfc1d97
Krait <krait@skbkontur.ru>
2018-09-27 15:56:43 +0500

    Cleaned up formatting and naming a little.

--- Vostok.Logging.Console/EventsWriting/ConsoleWriterFactory.cs
+++ Vostok.Logging.Console/EventsWriting/ConsoleWriterFactory.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.IO;
-using System.Text;
+﻿using System.IO;
 using Vostok.Logging.Console.Utilities;
 
 namespace Vostok.Logging.Console.EventsWriting
@@ -18,19 +16,16 @@ namespace Vostok.Logging.Console.EventsWriting
 
         public IConsoleWriter CreateWriter()
         {
-            var colorChanger = featuresDetector.AreColorsSupported ? 
-                new ConsoleColorChanger() as IConsoleColorChanger : 
-                new FakeConsoleColorChanger();
+            var colorChanger = featuresDetector.AreColorsSupported ? new ConsoleColorChanger() as IConsoleColorChanger : new FakeConsoleColorChanger();
 
-            var writer = ObtainWriter();
-            return new ConsoleWriter(writer, colorChanger);
+            return new ConsoleWriter(ObtainTextWriter(), colorChanger);
         }
 
-        private TextWriter ObtainWriter()
+        private TextWriter ObtainTextWriter()
         {
             if (OutputRedirectionDetector.IsOutputRedirected())
                 return System.Console.Out;
-            
+
             var stream = System.Console.OpenStandardOutput();
             return new StreamWriter(stream, System.Console.OutputEncoding, bufferSize, true) {AutoFlush = false};
         }
